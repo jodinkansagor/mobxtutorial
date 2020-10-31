bf0636c339d52ee35d2da4fefe677e2ced74e008
jodinkansagor <joedvice@gmail.com>
2020-10-31 13:48:47 -0700

    readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Class from "./pages/Class";
 import Function from "./pages/Function";
+import styles from './App'
 
 function App() {
   return (
     <>
-      <h1>Mushrooms!!</h1>
+      <h1 className={styles.title}>Mushrooms!!</h1>
       <Function />
       <h1>Let's have another list</h1>
       <Class />
